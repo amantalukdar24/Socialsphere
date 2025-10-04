657cddde6bafe1283e8e291d2319b7e496310603
amantalukdar24 <amantalukdar24@gmail.com>
2025-10-04 13:20:50 +0530

    user1 controllers updated

--- backend/controllers/user1.js
+++ backend/controllers/user1.js
@@ -43,35 +43,43 @@ const changePassword=async (req,res)=>{
         return res.status(500).json("Internal Server Error");
     }
 }
-const deleteAccount=async (req,res)=>{
+const deleteAccount = async (req, res) => {
     try {
-        const userId=req.user._id;
-        await POST.deleteMany({
-            userId:userId
-        });
-       await LIKE.deleteMany({
-        userId:userId
-       });
-       await COMMENT.deleteMany({
-         userId:userId
-       });
-       await STORY.deleteMany({
-        userId:userId,
-       });
-       const user=await USER.findById(userId);
-       const {followers}=user;
-       await Promise.all(followers.map(async (user)=>{
-         await USER.findByIdAndUpdate(user,{$pull:{following:userId,followers:userId}});
-       }));
-       await USER.findByIdAndDelete(userId);
-       return res.status(200).json({success:true,mssg:"Account Delete"});
-       
-        
+      const userId = req.user._id;
+  
+      
+      await Promise.all([
+        POST.deleteMany({ userId }),
+        LIKE.deleteMany({ userId }),
+        COMMENT.deleteMany({ userId }),
+        STORY.deleteMany({ userId }),
+      ]);
+  
+      const user = await USER.findById(userId);
+      if (!user) return res.status(404).json({ success: false, mssg: "User not found" });
+  
+      const { followers, following } = user;
+  
+      
+      await Promise.all([
+        ...followers.map(followerId =>
+          USER.findByIdAndUpdate(followerId, { $pull: { following: userId } })
+        ),
+        ...following.map(followingId =>
+          USER.findByIdAndUpdate(followingId, { $pull: { followers: userId } })
+        ),
+      ]);
+  
+      
+      await USER.findByIdAndDelete(userId);
+  
+      return res.status(200).json({ success: true, mssg: "Account deleted successfully" });
     } catch (err) {
-        console.log(`${err}`)
-        return res.status(500).json({success:false,mssg:"Internal Server Error"});
+      console.error(err);
+      return res.status(500).json({ success: false, mssg: "Internal Server Error" });
     }
-}
+  };
+  
 module.exports={changeUsername,
     changePassword,
     deleteAccount,
